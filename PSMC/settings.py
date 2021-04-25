5d9a437075b97f3194959fa955c582715c4b0c11
Steven Nodalo <s.nodalo656@student.nu.edu>
2021-04-25 16:15:30 -0700

    Updated setting.py Debug False

--- PSMC/settings.py
+++ PSMC/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = secrets.get_secret()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
