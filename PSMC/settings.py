b3e56b19a6cc9004ca4f08d19bc3d012bf7f3683
Steven Nodalo <s.nodalo656@student.nu.edu>
2021-04-25 19:17:51 -0700

    testing2

--- PSMC/settings.py
+++ PSMC/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import register.apps
 from PSMC import secrets
 import dj_database_url
+import django_heroku
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -144,3 +145,5 @@ db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+django_heroku.settings(locals())
