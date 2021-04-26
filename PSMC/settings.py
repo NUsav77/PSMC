073e60ac8871e6691728213879bd4ab8a046892e
Steven Nodalo <s.nodalo656@student.nu.edu>
2021-04-25 18:00:57 -0700

    settings

--- PSMC/settings.py
+++ PSMC/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = secrets.get_secret()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['savblock.herokuapp.com']
+ALLOWED_HOSTS = ['savblock.herokuapp.com', '127.0.0.1']
 # ALLOWED_HOSTS = []
 
 # Application definition
@@ -136,7 +136,10 @@ LOGOUT_REDIRECT_URL = 'login'
 
 AUTH_USER_MODEL = 'SavBlock.User'
 
+
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
