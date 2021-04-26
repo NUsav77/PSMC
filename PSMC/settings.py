5e50417b235a5e6fa2aa50f4a50e7348daf76715
Steven Nodalo <s.nodalo656@student.nu.edu>
2021-04-25 20:18:56 -0700

    settings update

--- PSMC/settings.py
+++ PSMC/settings.py
@@ -25,8 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = secrets.get_secret()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
+DEBUG = True
 ALLOWED_HOSTS = ['savblock.herokuapp.com']
 # ALLOWED_HOSTS = []
 
